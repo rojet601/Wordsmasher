527caeb394f48746f3f03bcb3f20f46ded4481e4
Daniel Matus <dmatus1306@gmail.com>
2013-03-23 17:40:40 +0100

    Ruckeln und Größe

--- src/Display.java
+++ src/Display.java
@@ -1,11 +1,15 @@
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
 
 
 public class Display extends Canvas {
 	private static final long serialVersionUID = 8274011568777903027L;
 	private Spiel spiel;
+	
+	private Image bufImage;
+	private Graphics bufG;
 	
 	public Display(Spiel spiel) {
 		this.spiel = spiel;
@@ -18,10 +22,29 @@ public class Display extends Canvas {
 		
 		for(Teil teil : spiel.teile) {
 			g.setColor(teil.farbe);
-			g.fillOval(teil.x, teil.y, 50, 30);
+			g.fillOval(teil.x, teil.y, 100, 60);
 		}
 		
 		g.setColor(Color.GRAY);
 		g.drawRect(0, 0, this.getWidth()-1, this.getHeight()-1);
+	}
+	
+	public void update(Graphics g) { // Double-Buffering
+		int w = this.getSize().width;
+		int h = this.getSize().height;
+
+		if(bufImage == null) {
+			bufImage = this.createImage(w, h);
+			bufG = bufImage.getGraphics();
+		}
+
+		bufG.setColor(this.getBackground());
+		bufG.fillRect(0, 0, w, h);
+
+		bufG.setColor(this.getForeground());
+
+		paint(bufG);
+
+		g.drawImage(bufImage, 0, 0, this);
 	}
 }
